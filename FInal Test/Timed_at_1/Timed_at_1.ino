3871a6d94aadd0aa95ffba1db506a65b258ab6c8
Mitchell Reiff <mapr323@gmail.com>
2019-11-11 14:42:50 -0500

    Converted to Functions

--- FInal Test/Timed_at_1/Timed_at_1.ino
+++ FInal Test/Timed_at_1/Timed_at_1.ino
@@ -7,6 +7,9 @@
 Servo left; 
 Servo right;
 
+int leftTime = 0;
+int rightTime = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,138 +20,148 @@ void setup() {
   right.attach(motor2pin);
   delay(3000);
 
-//forward
-    left.write(130);
-    right.write(20);
-    delay(500);
-
-//stop
-    left.write(90);
-    right.write(90);
-
-    delay(500);
-
-//right turn
-    right.write(90);
-    left.write(130);
-    delay(340);
-
-//stop
-    left.write(90);
-
-    delay(300);
-    
-//forward to B
-    right.write(20);
-    left.write(130);
-//    delay(872);
-    delay(800);
+forward(500);
+halt(500);
+turnRight();
+halt(300);
+forward(800); //to B
+halt(400); //stop B
+backwards(240);
+halt(400);
+turnLeft();
+halt(400);
+forward(800);
+halt(400);
+turnLeft();
+halt(600);
+forward(680); //to D
+halt(600);
+backwards(340);
+halt(400);
+turnArround();
+halt(400);
+forward(800); //to A
+halt(400); //stop A
 
 
-//stop at B
-    right.write(90);
-    left.write(90);
-    delay(400);
 
-//back up
-    right.write(110);
-    left.write(60);
-    delay(240);
-
-  //stop
-  left.write(90);
-  right.write(90);
-
-  delay(400);
-
-  //left turn
-  right.write(20);
-  left.write(90);
-  delay(400);
-
-  //stop
-  right.write(90);
-  delay(400);
-
-  //forward
-  right.write(20);
-  left.write(130);
-  delay(800);
-
-  //stop
- right.write(90);
- left.write(90);
-
- delay(400);
-
- //turn left
-   right.write(20);
-  left.write(90);
-  delay(410);
-
-  //stop
-  right.write(90);
-  left.write(90);
-  delay(600);
-
-  //forward to D
-  right.write(20);
-left.write(130);
-delay(680);
-
-  //stop
-  right.write(90);
-  left.write(90);
-
-  delay(600);
-
-//for (int i=0; i<8; i++){
-//
-//  //back to A
-//  right.write(120);
-//left.write(60);
-//delay(240);
+////forward
+//    left.write(130);
+//    right.write(20);
+//    delay(500);
 //
 ////stop
-//right.write(90);
-//left.write(90);
+//    left.write(90);
+//    right.write(90);
 //
-//delay(500);
+//    delay(500);
+//
+////right turn
+//    right.write(90);
+//    left.write(130);
+//    delay(340);
+//
+////stop
+//    left.write(90);
+//
+//    delay(300);
+//    
+////forward to B
+//    right.write(20);
+//    left.write(130);
+////    delay(872);
+//    delay(800);
+//
+//
+////stop at B
+//    right.write(90);
+//    left.write(90);
+//    delay(400);
+//
+////back up
+//    right.write(110);
+//    left.write(60);
+//    delay(240);
+
+//  //stop
+//  left.write(90);
+//  right.write(90);
+//
+//  delay(400);
+
+//  //left turn
+//  right.write(20);
+//  left.write(90);
+//  delay(400);
+
+//  //stop
+//  right.write(90);
+//  delay(400);
+
+//  //forward
+//  right.write(20);
+//  left.write(130);
+//  delay(800);
+
+//  //stop
+// right.write(90);
+// left.write(90);
+//
+// delay(400);
+//
+// //turn left
+//   right.write(20);
+//  left.write(90);
+//  delay(410);
+
+//  //stop
+//  right.write(90);
+//  left.write(90);
+//  delay(600);
+//
+//  //forward to D
+//  right.write(20);
+//left.write(130);
+//delay(680);
+//
+//  //stop
+//  right.write(90);
+//  left.write(90);
+//
+//  delay(600);
+
+
+////back up
+//    right.write(110);
+//    left.write(60);
+//    delay(340);
+
+//  //stop
+//  left.write(90);
+//  right.write(90);
+//
+//  delay(400);
+
 //  
-//}
+////turn arround
+//    right.write(90);
+//    left.write(130);
+//    delay(875);
 
+////stop
+//    right.write(90);
+//    left.write(90);
+//    delay(400);
 
-//back up
-    right.write(110);
-    left.write(60);
-    delay(340);
-
-  //stop
-  left.write(90);
-  right.write(90);
-
-  delay(400);
-
-  
-//turn arround
-    right.write(90);
-    left.write(130);
-    delay(875);
-
-//stop
-    right.write(90);
-    left.write(90);
-    delay(400);
-
-    //forward to A
-
-    right.write(20);
-    left.write(130);
-    delay(800);
-
-    //stop
-    right.write(90);
-    left.write(90);
+//    //forward to A
+//
+//    right.write(20);
+//    left.write(130);
+//    delay(800);
+//
+//    //stop
+//    right.write(90);
+//    left.write(90);
 
 
 
@@ -161,35 +174,54 @@ void loop() {
 
 }
 
-//void forward(){
-//
-//right.print(20);
-//left.print(150);
-//  
-//}
-//
-//void backwards(){
-//
-//right.print(110);
-//left.print(60);
-//  
-//}
-//
-//void turnLeft(){
-//    right.write(20);
-//    left.write(90);
-//  
-//}
-//
-//void turnRight(){
-//    right.write(90);
-//    left.write(130);
-//  
-//}
-//
-//void halt(){
-//
-//left.write(90);
-//right.write(90);
-//  
-//}
+void forward(int t){
+    
+    right.write(20);
+    left.write(150);
+    delay(t);
+    right.write(90);
+    left.write(90);
+  
+}
+
+void backwards(int t){
+    
+    right.write(110);
+    left.write(60);
+    delay(t);
+    right.write(90);
+    left.write(90);
+  
+}
+
+void turnLeft(){
+    right.write(20);
+    left.write(90);
+    delay(leftTime);
+    right.write(90);
+    left.write(90);
+  
+}
+
+void turnRight(){
+    right.write(90);
+    left.write(130);
+    delay(rightTime);
+    right.write(90);
+    left.write(90);
+  
+}
+
+void halt(int t){
+
+    left.write(90);
+    right.write(90);
+    delay(t);
+  
+}
+
+void turnArround(){
+    right.write(90);
+    left.write(130);
+    delay(875);
+}
